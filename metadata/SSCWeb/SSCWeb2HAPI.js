093a72b19861162026f28fef03eccf1123e91c5e
KSR <ksreddy9945@gmail.com>
2020-09-08 18:58:13 -0400

    httttt

--- metadata/SSCWeb/SSCWeb2HAPI.js
+++ metadata/SSCWeb/SSCWeb2HAPI.js
@@ -128,17 +128,17 @@ function makeHAPI(jsonraw, cb) {
 			var type = paraminfo[5].replace(/"/g, "");
 			//console.log(catalog[i]["info"]["parameters"][j]["type"],type,parseInt(type.replace(/%|s/,"")));
 
-			if (type.substring(type.length - 2, type.length-1) === "f") {
-				catalog[i]["info"]["parameters"][j]["type"] = "double";
-			}
-			if (type.substring(type.length - 2, type.length-1) === "d") {
-				catalog[i]["info"]["parameters"][j]["type"] = "integer";
-			}
-			if (type.substring(type.length - 2, type.length-1) === "s") {
-				let len = parseInt(type.replace(/%|s/, ""));
-				catalog[i]["info"]["parameters"][j]["type"] = "string";
-				catalog[i]["info"]["parameters"][j]["length"] = len;
-			}
+			if (/f$/.test(type)) {
+					catalog[i]["info"]["parameters"][j]["type"] = "double";
+				}
+				if (/d$/.test(type)) {
+					catalog[i]["info"]["parameters"][j]["type"] = "integer";
+				}
+				if (/s$/.test(type)) {
+					let len = parseInt(type.replace(/%|s/,""));
+					catalog[i]["info"]["parameters"][j]["type"] = "string";
+					catalog[i]["info"]["parameters"][j]["length"] = len;
+				}
 		}
 
 		var Time = {
